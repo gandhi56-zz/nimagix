d5df512ebc5d64273add2ec32627b078b6a63edb
Anshil Gandhi <gandhi21299@gmail.com>
2021-05-01 12:39:08 -0600

    mario can run now, but only in-place

--- src/player.rs
+++ src/player.rs
@@ -1,21 +1,23 @@
 use bevy::prelude::*;
 
-pub struct Player<'a>{
-    name: &'a str,
+struct Name(String);
+pub struct Player{
     state: PlayerState,
+    frame_idx: usize,
 }
 
+#[derive(Debug)]
 pub enum PlayerState{
-    Idle_Left,
-    Idle_Right,
-    Moving_Right,
-    Moving_Left,
+    IdleLeft,
+    IdleRight,
+    MovingRight,
+    MovingLeft,
     // TODO #3 add more states like jumping, flying, growing, etc.
 }
 
-impl Player<'static>{
-    pub fn new() -> Player<'static>{
-        Player{name: "mario", state: PlayerState::Idle_Right}
+impl Player{
+    pub fn new() -> Player{
+        Player{state: PlayerState::IdleRight, frame_idx: 0}
     }
 }
 
@@ -24,6 +26,7 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin{
     fn build(&self, app: &mut AppBuilder){
         app.add_startup_system(setup.system())
+            .add_system(keyboard_player_system.system())
             .add_system(animate_player.system());
     }
 }
@@ -34,9 +37,9 @@ fn setup(
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ){
     let tile_size = Vec2::splat(12.);
-    let tile_scale = Vec2::splat(6.);
+    let tile_scale = Vec2::new(4.0, 3.0);
     let texture_handle = assets.load("graphics/smb_mario_sheet.png");
-    let texture_atlas = TextureAtlas::from_grid(texture_handle, tile_size, 14, 6);
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, tile_size, 14, 12);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
     
     // commands.spawn_bundle(OrthographicCameraBundle::new_2d());
@@ -45,19 +48,60 @@ fn setup(
         transform: Transform::from_scale(tile_scale.extend(0.0)),
         ..Default::default()
     })
-    .insert(Timer::from_seconds(0.1, true));
+    .insert(Timer::from_seconds(0.1, true))
+    .insert(Player::new());
 }
 
 fn animate_player(
     time: Res<Time>,
-    texture_atlases: Res<Assets<TextureAtlas>>,
-    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<TextureAtlas>)>
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite, &Handle<bevy::prelude::TextureAtlas>)>,
+    mut player: ResMut<Player>,
 ){
-    for (mut timer, mut sprite, texture_atlas_handle) in query.iter_mut(){
+    // sprite indices
+    let idle_right_sprites = [7u32];
+    let idle_left_sprites = [6u32];
+    let moving_right_sprites = [8u32, 9, 10, 11];
+    let moving_left_sprites = [5u32, 4, 3, 2];
+
+    for (mut timer, mut sprite, _texture_atlas_handle) in query.iter_mut(){
         timer.tick(time.delta());
         if timer.finished(){
-            let texture_atlas = texture_atlases.get(texture_atlas_handle).unwrap();
-            sprite.index = ((sprite.index as usize + 1) % texture_atlas.textures.len()) as u32;
+            match &player.state{
+                PlayerState::IdleRight => {
+                    sprite.index = idle_right_sprites[0];
+                }
+                PlayerState::IdleLeft => {
+                    sprite.index = idle_left_sprites[0];
+                }
+                PlayerState::MovingRight => {
+                    sprite.index = moving_right_sprites[player.frame_idx];
+                    player.frame_idx = (player.frame_idx + 1) % moving_right_sprites.len();
+                }
+                PlayerState::MovingLeft => {
+                    sprite.index = moving_left_sprites[player.frame_idx];
+                    player.frame_idx = (player.frame_idx + 1) % moving_left_sprites.len();
+                }
+            }
         }
+    }
+}
+
+fn keyboard_player_system(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut player: ResMut<Player>,
+){
+    if keyboard_input.pressed(KeyCode::D){
+        player.state = PlayerState::MovingRight;
+    }
+    else if keyboard_input.pressed(KeyCode::A){
+        player.state = PlayerState::MovingLeft;
+    }
+    else if keyboard_input.just_released(KeyCode::D){
+        player.state = PlayerState::IdleRight;
+        player.frame_idx = 0;
+    }
+    else if keyboard_input.just_released(KeyCode::A){
+        player.state = PlayerState::IdleLeft;
+        player.frame_idx = 0;
     }
 }
